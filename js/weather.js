0684dff8382b648133caf9c485d672884f125508
narin111 <jungnr11@naver.com>
2021-12-02 02:00:15 +0900

    js fin

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,15 @@ const API_KEY="df4e43ded5564675ab4ceb4047fff654";
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-     fetch(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data)=>{
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 
 function onGeoError(){
